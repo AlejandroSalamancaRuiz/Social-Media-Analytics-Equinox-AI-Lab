b429827b568b3e027a0a15942c55852f19a71b9d
Alejandro Salamanca Ruiz <99615180+AlejandroSalamancaRuiz@users.noreply.github.com>
2024-10-22 10:55:02 +0100

    Update config.py

--- Back/config.py
+++ Back/config.py
@@ -1,7 +1,7 @@
 
 uri_neo = 'neo4j://localhost:7687'
 user_neo = 'neo4j'
-password_neo ='mcplus23'
+password_neo ='xxx'
 
 
-mongo_client = "mongodb://marketing-db:0ZTwJOqrbGXpBWr7U2fbKmBxHmP4soFYUka0eECuqX8O4fUfzNBVuNUlqyCnUIA1iyWZVn6RKggjK9PZpPWwbA==@marketing-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@marketing-db@"
+mongo_client = "xxx"
